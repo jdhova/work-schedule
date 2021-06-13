58a9c98b2368bf7c7afcb825b1b8e6bf7a2a1929
Jude Okagu <juud0010@gmail.com>
2021-06-13 14:23:11 +0200

    forms

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useRef } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Form, Button, Col, Container, Row } from 'react-bootstrap';
 import Header from './header';
-
+import { useForm } from 'react-hook-form';
 import './App.css';
 
 function App() {
@@ -11,21 +11,34 @@ function App() {
     // 'obum',
     // 'grace',
     // 'don',
-    { firstName: 'John', lastName: 'Doe', age: 50, eyeColor: 'blue' },
-    { firstName: 'Juud', lastName: 'cole', age: 40, eyeColor: 'grlue' },
+    //{ firstName: 'John', lastName: 'Doe', age: 50, eyeColor: 'blue' },
+    //{ firstName: 'Juud', lastName: 'cole', age: 40, eyeColor: 'grlue' },
   ]);
+
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
     <div className='App'>
       <div className='main'>
         <>
           <Header names={names} />
           <Container>
-            <Form>
+            <Form onSubmit={handleSubmit(onSubmit)}>
               <Row>
                 <Col lg={4} md={6} sm={8}>
                   <Form.Group controlId='formBasicEmail'>
                     <Form.Label> Name</Form.Label>
-                    <Form.Control type='text' placeholder='Enter Name' />
+                    <Form.Control
+                      input
+                      type='text'
+                      placeholder='Enter Name'
+                      name='name'
+                      {...register('name', { required: true })}
+                    />
                     <Form.Text className='text-muted'></Form.Text>
                   </Form.Group>
                 </Col>
@@ -35,7 +48,14 @@ function App() {
                 <Col lg={4} md={6} sm={8}>
                   <Form.Group controlId='formBasicEmail'>
                     <Form.Label>Position</Form.Label>
-                    <Form.Control type='text' placeholder='Enter Postion' />
+                    <Form.Control
+                      input
+                      type='text'
+                      placeholder='Enter Postion'
+                      name='position'
+                      {...register('position', { required: true })}
+                      //ref={register}
+                    />
                     <Form.Text className='text-muted'></Form.Text>
                   </Form.Group>
                 </Col>
@@ -45,7 +65,13 @@ function App() {
                 <Col lg={4} md={6} sm={8}>
                   <Form.Group controlId='formBasicEmail'>
                     <Form.Label>Date</Form.Label>
-                    <Form.Control type='date' placeholder='Enter Date' />
+                    <Form.Control
+                      input
+                      type='date'
+                      placeholder='Enter Date'
+                      name='date'
+                      {...register('date', { required: true })}
+                    />
                     <Form.Text className='text-muted'></Form.Text>
                   </Form.Group>
                 </Col>
@@ -55,7 +81,14 @@ function App() {
                 <Col lg={2} md={3} sm={4}>
                   <Form.Group controlId='formBasicEmail'>
                     <Form.Label>From</Form.Label>
-                    <Form.Control type='time' placeholder='From - To' />
+                    <Form.Control
+                      input
+                      type='time'
+                      placeholder='From - To'
+                      name='from'
+                      {...register('from', { required: true })}
+                    />
+
                     <Form.Text className='text-muted'></Form.Text>
                   </Form.Group>
                 </Col>
@@ -63,7 +96,14 @@ function App() {
                 <Col lg={2} md={3} sm={4}>
                   <Form.Group controlId='formBasicEmail'>
                     <Form.Label>To</Form.Label>
-                    <Form.Control type='time' placeholder='From - To' />
+                    <Form.Control
+                      input
+                      type='time'
+                      placeholder='From - To'
+                      name='to'
+                      {...register('to', { required: true })}
+                    />
+
                     <Form.Text className='text-muted'></Form.Text>
                   </Form.Group>
                 </Col>
@@ -73,7 +113,13 @@ function App() {
                 <Col lg={3} md={4} sm={5}>
                   <Form.Group controlId='formBasicEmail'>
                     <Form.Label>Responsibilities</Form.Label>
-                    <Form.Control type='text' placeholder='Roles' />
+                    <Form.Control
+                      input
+                      type='text'
+                      placeholder='Task 1'
+                      name='task1'
+                      {...register('task1', { required: true })}
+                    />
                     <Form.Text className='text-muted'></Form.Text>
                   </Form.Group>
                 </Col>
@@ -81,7 +127,13 @@ function App() {
                 <Col lg={3} md={4} sm={5}>
                   <Form.Group controlId='formBasicEmail'>
                     <Form.Label>Responsibilities</Form.Label>
-                    <Form.Control type='text' placeholder='Roles' />
+                    <Form.Control
+                      input
+                      type='text'
+                      placeholder='Task 2'
+                      name='task2'
+                      {...register('task2', { required: true })}
+                    />
                     <Form.Text className='text-muted'></Form.Text>
                   </Form.Group>
                 </Col>
@@ -90,14 +142,26 @@ function App() {
               <Row>
                 <Col lg={3} md={4} sm={5}>
                   <Form.Group controlId='formBasicEmail'>
-                    <Form.Control type='texxt' placeholder='Roles' />
+                    <Form.Control
+                      input
+                      type='text'
+                      placeholder='Task 3'
+                      name='task3'
+                      {...register('task3', { required: true })}
+                    />
                     <Form.Text className='text-muted'></Form.Text>
                   </Form.Group>
                 </Col>
 
                 <Col lg={3} md={4} sm={5}>
                   <Form.Group controlId='formBasicEmail'>
-                    <Form.Control type='text' placeholder='Roles' />
+                    <Form.Control
+                      input
+                      type='text'
+                      placeholder='Task 4'
+                      name='task4'
+                      {...register('task4', { required: true })}
+                    />
                     <Form.Text className='text-muted'></Form.Text>
                   </Form.Group>
                 </Col>
@@ -106,20 +170,36 @@ function App() {
               <Row>
                 <Col lg={3} md={4} sm={5}>
                   <Form.Group controlId='formBasicEmail'>
-                    <Form.Control type='text' placeholder='Roles' />
+                    <Form.Control
+                      input
+                      type='text'
+                      placeholder='Task 5'
+                      name='task5'
+                      {...register('task5', { required: true })}
+                    />
                     <Form.Text className='text-muted'></Form.Text>
                   </Form.Group>
                 </Col>
 
                 <Col lg={3} md={4} sm={5}>
                   <Form.Group controlId='formBasicEmail'>
-                    <Form.Control type='texxt' placeholder='Roles' />
+                    <Form.Control
+                      input
+                      type='text'
+                      placeholder='Task 6'
+                      name='task6'
+                      {...register('task6', { required: true })}
+                    />
                     <Form.Text className='text-muted'></Form.Text>
                   </Form.Group>
                 </Col>
               </Row>
 
-              <Button variant='primary' type='submit'>
+              <Button
+                //onSubmit={handleSubmit(onSubmit)}
+                variant='primary'
+                type='submit'
+              >
                 Submit
               </Button>
             </Form>
