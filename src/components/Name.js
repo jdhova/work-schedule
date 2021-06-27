fbadfcb5b3390ae1cf07bd38b7526a26563c3be3
Jude Okagu <juud0010@gmail.com>
2021-06-27 23:36:15 +0200

    tables

--- src/components/Name.js
+++ src/components/Name.js
@@ -1,31 +1,41 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Form, Button, Col, Container, Row, Table } from 'react-bootstrap';
 
 const Name = ({ singleValue }) => {
   return (
     <div>
-      <h3>info</h3>
-      {singleValue.name}
-      <br></br>
-      {singleValue.position}
-      <br></br>
-      {singleValue.date}
-      <br></br>
-      {singleValue.from}
-      <br></br>
-      {singleValue.to}
-      <br></br>
-      {singleValue.task1}
-      <br></br>
-      {singleValue.task2}
-      <br></br>
-      {singleValue.task3}
-      <br></br>
-      {singleValue.task4}
-      <br></br>
-      {singleValue.task5}
-      <br></br>
-      {singleValue.task6}
-      <br></br>
+      <h3>Employee Task</h3>
+
+      <Table>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Username</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td colSpan='2'>Larry the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </Table>
     </div>
   );
 };
